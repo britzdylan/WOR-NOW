eea80ab435edc4ff7d316d111c314606efa70fc9
Dylan Britz <47594494+britzdylan@users.noreply.github.com>
2020-06-20 21:15:58 +0200

    replaced componentdidmount with useEffect

--- components/mainLayout/layout.js
+++ components/mainLayout/layout.js
@@ -2,7 +2,7 @@ import MobilenNav from './mobileNav';
 import Header from './header';
 import Footer from './footer';
 import { initGA, logPageView } from "../googleAnalytics"
-
+import React, { useEffect } from 'react'
 import { AppProvider } from '../context/appContext'
 import client from "../ApolloClient";
 import { ApolloProvider } from 'react-apollo';
@@ -10,13 +10,13 @@ import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks';
 
 
 const Layout = (props) => {
-    componentDidMount () {
+    useEffect(() => {
         if (!window.GA_INITIALIZED) {
           initGA()
           window.GA_INITIALIZED = true
         }
         logPageView()
-      }
+      })
     
     return (
         <AppProvider>
