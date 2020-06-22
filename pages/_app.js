7660195855736660367402c276830510d6fb64b4
Dylan Britz <britzdylan@gmail.com>
2020-06-22 12:22:09 +0200

    fixed MD

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ import { AppContext } from "../components/context/appContext";
 export default function MyApp(props) {
   const { Component, pageProps } = props;
   const { value4 } = React.useContext(AppContext);
-  const [product,setProduct] = React.useState("");
+  const [product, setProduct] = React.useState("");
   React.useEffect(() => {
     setProduct(localStorage.getItem('productData'));
     // Remove the server-side injected CSS.
@@ -24,33 +24,33 @@ export default function MyApp(props) {
     }
   }, []);
 
-  const jsonLD = ( data ) => {
-    const productData = data != "" ? JSON.parse(data)  : ""
-    const price =  data != "" ? productData.regularPrice.replace('R','') : "";
+  const jsonLD = (data) => {
+    const productData = data != "" ? JSON.parse(data) : ""
+    const price = data != "" ? productData.regularPrice.replace('R', '') : "";
     return {
       __html: productData != "" ?
-      ` {
+        ` {
         "@context":"https://schema.org",
-      "@type":"Product",
-       "productID": "${productData.id}",
+        "@type":"Product",
+        "productID": "${productData.productId}",
         "name":"${productData.name}",
-        "description":"Get the ${product.name} for only R ${price} at WorldofRugby with nation wide shipping and fast and secure online shopping.",
+        "description":"Get the ${productData.name} for only R ${price} at WorldofRugby with nation wide shipping and fast and secure online shopping.",
         "url":"https://www.worldofrugby.co.za/shop/product/${productData.slug}?id=${productData.productId}&type=${productData.__typename}",
         "image":"${productData.image.sourceUrl}",
-     "brand":"",
-     "google_product_category" : "1110",
+        "brand":"",
      "offers": [
        {
          "@type": "Offer",
          "price": "${price}",
         "priceCurrency": "ZAR",
          "itemCondition": "https://schema.org/NewCondition",
-         "availability": "https://schema.org/InStock"
+         "availability": "https://schema.org/InStock",
+         "google_product_category" : "1110"
        }
       ]
    }` : ""
     };
-    
+
 
   }
 
@@ -59,25 +59,25 @@ export default function MyApp(props) {
       <Head>
         <title>WorldofRugby</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"  rel="preload" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="preload" rel="stylesheet"></link>
 
         {/* favicon */}
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/manifest.json"/>
-        <meta name="msapplication-TileColor" content="#ffffff"/>
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff"></meta>
         <meta http-equiv="cache-control" content="max-age=43200" />
         <meta name="google-site-verification" content="KaBFEEM7SBm3hDtRv1iDVa_6EQkep4O-c1DBZjRxVTI" />
