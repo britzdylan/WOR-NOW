4ec95ab49cbe83e11071579671a9353d5278412a
Dylan Britz <britzdylan@gmail.com>
2020-07-22 11:38:01 +0200

    added hotjar tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -108,6 +108,18 @@ export default function MyApp(props) {
           
             gtag('config', 'AW-815167994');`}}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:1672069,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}} />
         {/* favicon */}
       </Head>
       <ThemeProvider theme={theme}>
