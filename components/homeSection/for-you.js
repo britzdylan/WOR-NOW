444ac6607b75a406d5b23772dbb8fbf5c2c59c7c
Dylan Britz <47594494+britzdylan@users.noreply.github.com>
2021-06-22 15:19:52 +0200

    banner removal

--- components/homeSection/for-you.js
+++ components/homeSection/for-you.js
@@ -1,49 +1,46 @@
-import CatPreview from '../global/category-preview'
-import InfoCard from './components/infoCards'
-import FeaturedItem from './components/featured-item'
-import Subrcibe from '../global/subscribe'
-import Hero from './components/hero'
-import { makeStyles } from '@material-ui/core/styles';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
-import CategoryBanner from '../homeSection/components/categoryBanner'
-import Button from '@material-ui/core/Button';
-import Link from 'next/link'
+import CatPreview from "../global/category-preview";
+import InfoCard from "./components/infoCards";
+import FeaturedItem from "./components/featured-item";
+import Subrcibe from "../global/subscribe";
+import Hero from "./components/hero";
+import { makeStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import CategoryBanner from "../homeSection/components/categoryBanner";
+import Button from "@material-ui/core/Button";
+import Link from "next/link";
 // Import Swiper React components
-import React from 'react';
+import React from "react";
 import Slider from "react-slick";
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    margin: '64px auto ',
-    maxWidth: '10%',
-    [theme.breakpoints.down('sm')]: {
-      margin: '24px auto'
+    margin: "64px auto ",
+    maxWidth: "10%",
+    [theme.breakpoints.down("sm")]: {
+      margin: "24px auto",
     },
   },
   rootA: {
-    display: 'none',
-    [theme.breakpoints.down('md')]: {
-      margin: '64px auto',
-      display: 'block'
+    display: "none",
+    [theme.breakpoints.down("md")]: {
+      margin: "64px auto",
+      display: "block",
     },
   },
   Banners: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
   },
   catBar: {
-    padding: '16px',
+    padding: "16px",
     margin: "16px 0",
     letterSpacing: "2px",
-    [theme.breakpoints.down('md')]: {
-      padding: '16px 32px'
+    [theme.breakpoints.down("md")]: {
+      padding: "16px 32px",
     },
-
-
   },
   catName: {
     margin: "0 16px",
@@ -51,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
     color: "#6b6b6b",
     textAlign: "center",
     fontWeight: "400",
-    '&::after': {
+    "&::after": {
       position: "relative",
       display: "block",
       width: "40px",
@@ -60,36 +57,52 @@ const useStyles = makeStyles((theme) => ({
       borderRadius: "50px",
       margin: "1rem auto",
       content: '" "',
-      transform: "rotate(-5deg)"
-    }
+      transform: "rotate(-5deg)",
+    },
   },
   catButton: {
     width: "100%",
     display: "flex",
     justifyContent: "center",
-    color: "black"
+    color: "black",
   },
   slider: {
-    margin: "24px 0"
-  }
+    margin: "24px 0",
+  },
 }));
-
-
-
 
 const forYou = (props) => {
   const classes = useStyles();
   const { products, saleItems, featItems } = props;
   const Onsale = true;
   const NotOnSale = false;
-  const banners = [ 
-    { src: "/banner1.jpg", link: "/shop/fan-gear/springboks?pageName=&page=1&curCursor=&field=DATE&sale=false&parentID=1090&isFeat=false&Order=DESC" }, 
-    { src: "/banner2.jpg", link: "/shop/fan-gear/british-and-irish-lions-2021?pageName=&page=1&curCursor=&field=DATE&sale=false&parentID=1183&isFeat=false&Order=DESC" },
-    { src: "/banner3.jpg", link: "/shop/fan-gear/canterbury-fan-gear?pageName=&page=1&curCursor=&field=DATE&sale=false&parentID=1185&isFeat=false&Order=DESC" },
-    { src: "/banner4.jpg", link: "https://www.cashbackworld.com/za/registration/partner/22301001" },
-    { src: "/banner5.jpg", link: "https://live.mobicred.co.za/cgi-bin/wspd_cgi.sh/WService=wsb_mcrliv/run.w?run=application&merchantID=4311&returnUrl=https://worldofrugby.co.za" },  
-    { src: "/banner6.jpg", link: "/shop/rugby-boots/adidas-boots?pageName=&page=1&curCursor=&field=DATE&sale=false&parentID=791&isFeat=false&Order=DESC" }
-  ]
+  const banners = [
+    {
+      src: "/banner1.jpg",
+      link:
+        "/shop/fan-gear/springboks?pageName=&page=1&curCursor=&field=DATE&sale=false&parentID=1090&isFeat=false&Order=DESC",
+    },
+    {
+      src: "/banner2.jpg",
+      link:
+        "/shop/fan-gear/british-and-irish-lions-2021?pageName=&page=1&curCursor=&field=DATE&sale=false&parentID=1183&isFeat=false&Order=DESC",
+    },
+    {
+      src: "/banner3.jpg",
+      link:
+        "/shop/fan-gear/canterbury-fan-gear?pageName=&page=1&curCursor=&field=DATE&sale=false&parentID=1185&isFeat=false&Order=DESC",
+    },
+    {
+      src: "/banner6.jpg",
+      link:
+        "/shop/rugby-boots/adidas-boots?pageName=&page=1&curCursor=&field=DATE&sale=false&parentID=791&isFeat=false&Order=DESC",
+    },
+    {
+      src: "/banner7.jpg",
+      link:
+        "/shop/rugby-boots/puma-boots?pageName=&page=1&curCursor=&field=DATE&sale=false&parentID=792&isFeat=false&Order=DESC",
+    },
+  ];
   const settings = {
     dots: true,
     infinite: true,
@@ -98,44 +111,122 @@ const forYou = (props) => {
     slidesToScroll: 1,
     autoplay: true,
     arrows: true,
-    pauseOnHover: true
+    pauseOnHover: true,
   };
   return (
     <div>
       {/* //<Hero banner={banner} /> */}
 
       <Slider className={classes.slider} {...settings}>
-        {banners.map((item, index) =>
+        {banners.map((item, index) => (
           <div index={index}>
             <Hero src={item.src} link={item.link} />
           </div>
-        )}
+        ))}
       </Slider>
 
       <InfoCard />
       <Divider className={classes.root} />
-      <div className={classes.catBar} >
-        <Typography className={classes.catName} align="center" variant="subtitle2">Top Picks</Typography>
+      <div className={classes.catBar}>
+        <Typography
+          className={classes.catName}
+          align="center"
+          variant="subtitle2"
+        >
+          Top Picks
+        </Typography>
       </div>
       <div className={classes.Banners}>
-        <CategoryBanner banner="sale" isFeat={false} sale={Onsale} parentID={216} parent={"all-products"} slug={"onsale"} filter={'DATE'} />
-        <CategoryBanner banner="popular" isFeat={false} sale={NotOnSale} parentID={216} parent={"all-products"} slug={"popular"} filter={'TOTAL_SALES'} />
+        <CategoryBanner
+          banner="sale"
+          isFeat={false}
+          sale={Onsale}
+          parentID={216}
+          parent={"all-products"}
+          slug={"onsale"}
+          filter={"DATE"}
+        />
+        <CategoryBanner
+          banner="popular"
+          isFeat={false}
+          sale={NotOnSale}
+          parentID={216}
+          parent={"all-products"}
+          slug={"popular"}
+          filter={"TOTAL_SALES"}
+        />
       </div>
-      <CatPreview products={featItems} catName="Current Sales" sale={NotOnSale} parentID={216} parent={"latest-products"} slug={"latest-products"} filter={'DATE'} />
-      <div className={classes.catButton} >
-        <Link href={{ pathname: `/shop/all-products/featured`, query: { page: `1`, curCursor: ``, field: `DATE`, sale: `false`, parentID: `216`, isFeat: `true`, Order: 'DESC' } }}><Button  >View More</Button></Link>
+      <CatPreview
+        products={featItems}
+        catName="Current Sales"
+        sale={NotOnSale}
+        parentID={216}
+        parent={"latest-products"}
+        slug={"latest-products"}
+        filter={"DATE"}
+      />
+      <div className={classes.catButton}>
+        <Link
+          href={{
+            pathname: `/shop/all-products/featured`,
+            query: {
+              page: `1`,
+              curCursor: ``,
+              field: `DATE`,
+              sale: `false`,
+              parentID: `216`,
+              isFeat: `true`,
+              Order: "DESC",
+            },
+          }}
+        >
+          <Button>View More</Button>
+        </Link>
       </div>
       <Divider className={classes.root} />
-      <CatPreview products={products} catName="Latest Arrivals" sale={NotOnSale} isFeat={false} parentID={216} parent={"latest-products"} slug={"latest-products"} filter={'DATE'} />
+      <CatPreview
+        products={products}
+        catName="Latest Arrivals"
+        sale={NotOnSale}
+        isFeat={false}
+        parentID={216}
+        parent={"latest-products"}
+        slug={"latest-products"}
+        filter={"DATE"}
+      />
       <Divider className={classes.root} />
-      <CatPreview products={saleItems} catName="Items On Sale" sale={Onsale} isFeat={false} parentID={216} parent={"latest-products"} slug={"latest-products"} filter={'MODIFIED'} />
-      <div className={classes.catButton} >
-        <Link href={{ pathname: `/shop/all-products/onsale`, query: { page: `1`, curCursor: ``, field: `DATE`, sale: `true`, parentID: `216`, isFeat: `false`, Order: 'DESC' } }}><Button  >View More</Button></Link>
+      <CatPreview
+        products={saleItems}
+        catName="Items On Sale"
+        sale={Onsale}
+        isFeat={false}
+        parentID={216}
+        parent={"latest-products"}
+        slug={"latest-products"}
+        filter={"MODIFIED"}
+      />
+      <div className={classes.catButton}>
+        <Link
+          href={{
+            pathname: `/shop/all-products/onsale`,
+            query: {
+              page: `1`,
+              curCursor: ``,
+              field: `DATE`,
+              sale: `true`,
+              parentID: `216`,
+              isFeat: `false`,
+              Order: "DESC",
+            },
+          }}
+        >
+          <Button>View More</Button>
+        </Link>
       </div>
 
       <Divider className={classes.rootA} />
-    </div >
-  )
-}
+    </div>
+  );
+};
 
 export default forYou;
